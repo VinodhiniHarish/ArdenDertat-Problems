a6df1b995c2d9a82e83ad5fe9a0869ed0c0c0889
Vino <vino@fileit.tax>
2017-09-08 13:42:19 -0700

    Anagrams

--- Ardendertat/Anagrams/src/Anagrams.java
+++ Ardendertat/Anagrams/src/Anagrams.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Anagrams {
 
 	public void findAnagrams(String str1, String str2) {
@@ -27,12 +27,60 @@ public class Anagrams {
 		}
 	} 
 	
+	
+	//Method2: with Hash Tables
+	public void findAnagrams2(String str1, String str2) {
+		str1 = str1.replaceAll("[^a-zA-Z]+", "");
+		str2 = str2.replaceAll("[^a-zA-Z]+", "");
+		str1 = str1.toLowerCase();
+		str2 = str2.toLowerCase();
+		HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+		boolean isAnagram = true;
+		for(int i=0;i<str1.length();i++) {
+			if(map.containsKey(str1.charAt(i))) {
+				map.put(str1.charAt(i), map.get(str1.charAt(i)) + 1);
+			}else {
+				map.put(str1.charAt(i), 1);
+			}
+		}
+		
+		for(int i=0;i<str2.length();i++) {
+			if(map.containsKey(str2.charAt(i))) {
+				int n = map.get(str2.charAt(i));
+				if(n >0)
+					map.put(str2.charAt(i),n-1);
+				else {
+					System.out.println("Strings are not anagrams");
+					isAnagram = false;
+					return;
+				}
+					
+			}else {
+				System.out.println("Strings are not anagrams");
+				isAnagram = false;
+				return;
+			}
+		}
+		
+		for(Map.Entry<Character,Integer> m : map.entrySet()) {
+			if(m.getValue()!=0) {
+				System.out.println("Strings are not anagrams");
+				isAnagram = false;
+				return;
+			}
+		}
+		if(isAnagram) {
+			System.out.println("The given Strings are anagrams");
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Anagrams a = new Anagrams();
 		String str1 = "Eleven :plus, two";
 		String str2 = "Tw,e,l.ve; plus one";
-		a.findAnagrams(str1, str2);
+//		a.findAnagrams(str1, str2);
+		a.findAnagrams2(str1, str2);
 	}
 
 }
